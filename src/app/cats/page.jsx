0c3422eaf46b26446543c5da6653e1680c9f5a05
KimPuro <eos002@naver.com>
2024-04-29 16:39:20 +0900

    feat : add main, search, cat page

---
+++ src/app/cats/page.jsx
@@ -0,0 +1,11 @@
+import ImagesWithDetails from "@/components/lists/RandomCats";
+import NewRandomCats from "@/components/Banners/NewRandomCats";
+
+const RandomCatsPage = () => {
+    return (<>
+        <NewRandomCats/>
+            <ImagesWithDetails/>
+</>
+    )
+}
+export default RandomCatsPage;
---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+"use client"
+import NotFound1 from "@/components/feedbacks/NotFound1";
+
+export default function NotFound() {
+    return (
+        <>
+            <NotFound1/>
+        </>
+    );
+}
